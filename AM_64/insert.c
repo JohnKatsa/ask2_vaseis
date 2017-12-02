1a9c7e8804c6075991f99c996b0dcd1c2009c5f0
Nikos <nikotheract11@gmail.com>
2017-12-02 16:52:19 +0200

    insert half-ready(?)

--- AM_64/insert.c
+++ AM_64/insert.c
@@ -114,53 +114,123 @@ void split(Stack stack, void* value1,void* value2){
 	void *upkey;
 
 	memcpy(&type,data,sizeof(char));	// block type
-	int i,offset=0;
-	for(i=0;i<=getKeysNumber(current)/2;i++){
-		if(cmp(....)) swap(t1,t2,l1,l2,value1,value2);
+	int i,offset;
+	if(type == 'd') {
+		offset = sizeof(char) + 2*sizeof(int);	// ignore type,keys_num,sibling
+		l1 = get_l1();	// ====================
+		l2 = get_l2();
+	}
+	else{
+	       offset = sizeof(char) + sizeof(int);
+	       l1 = sizeof(int);
+	       l2 = get_l1();
+	}
+	
+	int flag = 0;
+	void key,value,tempkey,tempvalue;
+
+	for(i=0;i<=(getKeysNumber(current)-1)/2;i++)	{	// ================   condition?? =================
+		memcpy(&key,&data[offset],l1);
+		offset += l1;
+		memcpy(&value,&data[offset],l2);
+		offset += l2;
+		if(cmp(....)){	// swap(t1,t2,l1,l2,value1,value2);
+			offset = offset - (l1 + l2);
+			flag = 1;
+			memcpy(&tempkey,&data[offset],l1);
+			offset += l1;
+			memcpy(&tempvalue,&data[offset],l2);
+			offset -= l1; // go back to overwrite data
+
+			memcpy(&data[offset],value1,l1);
+			offset += l1;
+			memcpy(&data[offset],value2,l2);
+			offset += l2;
+
+			memcpy(&key,&tempkey,l1);
+			memcpy(&value,&tempvalue,l2);
+
+			for(int j=i;j<=(getKeysNumber(current)-1)/2;j++){
+				memcpy(&tempkey,&data[offset],l1);
+				offset += l1;
+				memcpy(&tempvalue,&data[offset],l2);
+				offset -= l1; 
+
+				memcpy(&data[offset],&key,l1);
+				offset += l1;
+				memcpy(&data[offset],&value,l2);
+				offset += l2;
+
+				memcpy(&key,&tempkey,l1);
+				memcpy(&value,&tempvalue,l2);
+			}
+			break;
+		}
 	}
 	setKeysNumber(current,i);
-	
-	memcpy(upkey,data[offset1],l1);	// key to climb up after split
 
+	int offset1 = offset;
+	
 	BF_AllocateBlock(block);		// pote allazw current ????
 	char* data2 = BF_Block_GetData(block);
 
+	int blocks_num;
+	BF_GetBlockCounter(fileDesc,&blocks_num);
+	blocks_num--;
 	memcpy(data2,type,sizeof(char));
 	offset = sizeof(char);
 
 	int number = getKeysNumber(current)/2;
 	memcpy(&data2[offset],&number,sizeof(int));
 	offset += sizeof(int);
-	
+	if(type == 'd'){	
 	number = getRightSibling(current);
-
+	
 	memcpy(&data2[offset],&number,sizeof(int));
 	offset += sizeof(int);
 
-	number = getLeftSibling(current);
+	memcpy(&data[sizeof(char)+sizeof(int)],&blocks_num,sizeof(int)); // change sibling
+	}
+	if(type == 'd'){
+	memcpy(&data2[offset],&key,l1);			// ============================== mallon den xreizetai auto giati to key anebainei================================
+	offset += l1;
 
-	memcpy(&data2[offset],&number,sizeof(int));
-	offset += sizeof(int);
+	memcpy(&data2[offset],&value,l2);
+	offset += l2;
+	}
 
-	if(type == 'k') offset1 += l1 + sizeof(int);		// dont write twice upkey
+	for(i=(getKeysNumber(current)+1)/2;i<getKeysNumber(current);i++){  
+		if(flag == 0 && cmp(...)){
+			memcpy(&data[offset],value1,l1);
+			offset += l1;
+			memcpy(&data[offset],value2,l2);
+			offset += l2; 			// ========= need one more loop? =========
+			flag = 1;
 
-
-	for(i=getKeysNumber(current)/2+1;i<getKeysNumber(current);i++){  
-		if(type == 'k') offset1 += l1 + sizeof(int); 		
+		}
 		memcpy(&data2[offset],&data[offset1],l1);
 		offset += l1;
 		offset1 += l1;
 
-		if(type == 'k') {
-			memcpy(&data[offset],&data2[offset1],sizeof(int));
-			offset += sizeof(int);
-			offset1 += sizeof(int);
-		}
-		else {
-			memcpy(&data[offset],&data2[offset1],l2);
-			offset += l2;
-			offset1 += l2;
-		}
+		memcpy(&data2[offset],&data[offset1],l2);
+		offset += l2;
+		offset1 += l2;
+	}
+	if(current == getRoot()) {
+		BF_AllocateBlock(block);
+		data = BF_Block_GetData(block);
+		type = 'k';
+		memcpy(data,&type,sizeof(char));
+		offset = sizeof(char);
+
+		number = 1;	// one record
+		memcpy(&data[offset],&number,sizeof(int));
+		offset += sizeof(int);
+		BF_GetBlockCounter(fileDesc,&blocks_num);
+		insert(fileDesc,--blocks_num,&key,&(--blocks_num));		// =============== right values? ==============
+
+		setRoot(fileDesc,blocks_num);
+		return AME_OK;
 	}
 
 	current=stack.pop();
@@ -169,13 +239,48 @@ void split(Stack stack, void* value1,void* value2){
 	newblock--;	// last created block is te splited one
 	if(getKeysNumber(current) >= MAXKEYS) {
 		stack.push(current);
-		split(stack,value1,&newblock);
+		split(stack,&key,&newblock);
 	}
 	else insert(current,value1);
 
 
 	
+}
 
+
+void setKeysNumber(int fileDesc,int block_num,int new_num){
+	BF_Block *block;	// Init na min 3exasw
+	BF_GetBlock(fileDesc,block_num,block);
+	char *data = BF_Block_GetData(block);
+	int offset = sizeof(char);
+	
+	memcpy(&data[offset],&new_num,sizeof(int));
+
+}
+
+void setRoot(int fileDesc,int new_root){
+	BF_Block *block;
+	BF_GetBlock(fileDesc,0,block);
+	char *data = BF_Block_GetData(block);
+
+	int offset = strlen("B+Tree")+1+sizeof(int)*2 + sizeof(char)*2;
+
+	memcpy(&data[offset],&new_root,sizeof(int));
+}
+
+int getRightSibling(int fileDesc, int block_num){
+	BF_Block *block;        // Init na min 3exasw
+	BF_GetBlock(fileDesc,block_num,block);
+	char *data = BF_Block_GetData(block);
+	int offset = sizeof(char)+sizeof(int);
+	int sibling;
+	memcpy(&sibling,&data[offset],sizeof(int));
+	return sibling;
+}
+
+
+
+	
 
 
 
